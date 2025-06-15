65e25de90e8af461f78cc8c37dd737d53151d0e7
aiforkorea <aiforkorea@gmail.com>
2025-06-16 04:48:42 +0900

    1st major

--- apps/__init__.py
+++ apps/__init__.py
@@ -3,8 +3,8 @@ from flask import Flask
 from .config import Config
 
 def create_app():
-    #app = Flask(__name__)
-    app = Flask(__name__, static_folder='../apps/static')
+    app = Flask(__name__)
+    #app = Flask(__name__, static_folder='../apps/static')
     app.config.from_object(Config)
 
     # main 블루프린트 등록
