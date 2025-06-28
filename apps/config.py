804362306bfdff259f2e36a0c860585d361acb17
aiforkorea <aiforkorea@gmail.com>
2025-06-28 11:46:16 +0900

    base dir fix

--- apps/config.py
+++ apps/config.py
@@ -5,6 +5,7 @@ if os.environ.get('RENDER', None) != 'true':
     dotenv_path = os.path.join(BASE_DIR, '..', '.env')
     load_dotenv(dotenv_path)
 class Config:
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
     SECRET_KEY = os.getenv('SECRET_KEY')
     #API_KEY = os.getenv('API_KEY')
     IRIS_LABELS = ['setosa', 'versicolor', 'virginica']
